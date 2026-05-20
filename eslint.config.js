00c2e083b5c6a89b65239584c622f8d15866118b
João Paulo Gobira <jpgobira2@gmail.com>
2026-05-20 20:42:19 -0300

    add: configurando eslint

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import jestPlugin from 'eslint-plugin-jest';
+import globals from 'globals';
+
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...jestPlugin.environments.globals.globals,
+      },
+    },
+    plugins: {
+      jest: jestPlugin,
+    },
+    rules: {
+      ...jestPlugin.configs.recommended.rules,
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-conditional-expect': 'error',
+      'jest/no-identical-title': 'error',
+    },
+  },
+];
